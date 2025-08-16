442210f5293457d8f71085868a7a12fd5de1db7e
muhammed sinan <sinanzzsinu70@gmail.com>
2025-08-16 13:31:12 +0530

    Pro button added

--- app/src/main/java/com/example/lascadetask/screens/HomeScreen.kt
+++ app/src/main/java/com/example/lascadetask/screens/HomeScreen.kt
@@ -3,28 +3,57 @@ package com.example.lascadetask.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lascadetask.R
+import com.example.lascadetask.ui_components.ImgButtonFun
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen (){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.map_image),
-            contentDescription = "Map Image",
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent, // Transparent background
+                    scrolledContainerColor = Color.Transparent
+                ),
+                navigationIcon = {
+                    ImgButtonFun(resId = R.drawable.img,width = 87.dp,height = 46.dp, ps = 14.dp,)
+                },
+
+
+
+            )
+        }
+    ){ innerPadding ->
+        Box(
             modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.map_image),
+                contentDescription = "Map Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+
     }
 }
+
---
+++ app/src/main/java/com/example/lascadetask/ui_components/ImgButtonFun.kt
@@ -0,0 +1,21 @@
+package com.example.lascadetask.ui_components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+ fun ImgButtonFun(resId:Int, width: Dp, height: Dp, ps: Dp =0.dp, pe: Dp =0.dp, pt: Dp =0.dp, pb: Dp =0.dp) {
+    Image(
+        painter = painterResource(id = resId),
+        contentDescription = "",
+        modifier = Modifier
+            .size(width = width, height = height)
+            .padding(start =ps, bottom = pb, end = pe, top = pt )
+    )
+}
