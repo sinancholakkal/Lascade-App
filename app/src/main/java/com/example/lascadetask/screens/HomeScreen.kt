bb7a944859fefd35b529b33eadd4b1d36d275ee5
muhammed sinan <sinanzzsinu70@gmail.com>
2025-08-16 12:42:26 +0530

    background image added

--- app/src/main/java/com/example/lascadetask/screens/HomeScreen.kt
+++ app/src/main/java/com/example/lascadetask/screens/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.example.lascadetask.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.lascadetask.R
 
 @Composable
 fun HomeScreen (){
@@ -14,6 +20,11 @@ fun HomeScreen (){
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Hello home")
+        Image(
+            painter = painterResource(id = R.drawable.map_image),
+            contentDescription = "Map Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
     }
 }
